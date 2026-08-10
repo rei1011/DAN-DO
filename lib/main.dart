e281e10feed0ddf78badef929744d77c7dfdb349
rei1011 <r.mizobuchi3@gmail.com>
2026-08-10 18:45:42 +0900

    Advanced UI features

--- lib/main.dart
+++ lib/main.dart
@@ -1,172 +1,19 @@
-import 'dart:convert';
-import 'dart:io';
+import 'package:flutter/cupertino.dart';
 
-import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-
-import 'summary.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class RolodexApp extends StatelessWidget {
+  const RolodexApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ArticleView());
-  }
-}
-
-class ArticleView extends StatefulWidget {
-  const ArticleView({super.key});
-
-  @override
-  State<ArticleView> createState() => _ArticleViewState();
-}
-
-class _ArticleViewState extends State<ArticleView> {
-  final ArticleViewModel viewModel = ArticleViewModel(ArticleModel());
-
-  @override
-  void initState() {
-    super.initState();
-    viewModel.fetchArticle();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Wikipedia Flutter')),
-      body: ArticleBody(viewModel: viewModel),
-    );
-  }
-}
-
-class ArticleBody extends StatelessWidget {
-  const ArticleBody({super.key, required this.viewModel});
-
-  final ArticleViewModel viewModel;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListenableBuilder(
-      listenable: viewModel,
-      builder: (context, _) {
-        return switch ((
-          viewModel.isLoading,
-          viewModel.summary,
-          viewModel.error,
-        )) {
-          (true, _, _) => const CircularProgressIndicator(),
-          (_, _, final Exception e) => Text('Error: $e'),
-          (_, final summary?, _) => ArticlePage(
-            summary: summary,
-            nextArticleCallback: viewModel.fetchArticle,
-          ),
-          _ => const Text('Something went wrong!'),
-        };
-      },
-    );
-  }
-}
-
-class ArticlePage extends StatelessWidget {
-  const ArticlePage({
-    super.key,
-    required this.summary,
-    required this.nextArticleCallback,
-  });
-
-  final Summary summary;
-  final VoidCallback nextArticleCallback;
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ArticleWidget(summary: summary),
-          ElevatedButton(
-            onPressed: nextArticleCallback,
-            child: const Text('Next random article'),
-          ),
-        ],
+    return const CupertinoApp(
+      title: 'Rolodex',
+      theme: CupertinoThemeData(
+        barBackgroundColor: CupertinoDynamicColor.withBrightness(
+          color: Color(0xFFF9F9F9),
+          darkColor: Color(0xFF1D1D1D),
+        ),
       ),
+      home: CupertinoPageScaffold(child: Center(child: Text('Hello Rolodex!'))),
     );
-  }
-}
-
-class ArticleWidget extends StatelessWidget {
-  const ArticleWidget({super.key, required this.summary});
-
-  final Summary summary;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: Column(
-        spacing: 10,
-        children: [
-          if (summary.hasImage) Image.network(summary.originalImage!.source),
-          Text(
-            summary.titles.normalized,
-            overflow: TextOverflow.ellipsis,
-            style: Theme.of(context).textTheme.displaySmall,
-          ),
-          if (summary.description != null)
-            Text(
-              summary.description!,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          Text(summary.extract),
-        ],
-      ),
-    );
-  }
-}
-
-class ArticleModel {
-  Future<Summary> getRandomArticleSummary() async {
-    final uri = Uri.https(
-      'en.wikipedia.org',
-      '/api/rest_v1/page/random/summary',
-    );
-    final response = await get(uri);
-
-    if (response.statusCode != 200) {
-      throw const HttpException('Failed to update resource');
-    }
-
-    return Summary.fromJson(jsonDecode(response.body) as Map<String, Object?>);
-  }
-}
-
-class ArticleViewModel extends ChangeNotifier {
-  final ArticleModel model;
-  Summary? summary;
-  Exception? error;
-  bool isLoading = false;
-
-  ArticleViewModel(this.model);
-
-  Future<void> fetchArticle() async {
-    isLoading = true;
-    notifyListeners();
-    try {
-      summary = await model.getRandomArticleSummary();
-      print('Article loaded: ${summary!.titles.normalized}');
-      error = null;
-    } on HttpException catch (e) {
-      print('Error loading article: ${e.message}');
-      error = e;
-      summary = null;
-    }
-
-    isLoading = false;
-    notifyListeners();
   }
 }
