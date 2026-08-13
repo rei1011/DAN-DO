b7d02f0c7522a15fc75fed62763c76ac4266a6b7
rei1011 <r.mizobuchi3@gmail.com>
2026-08-13 13:55:37 +0900

    アニメーションの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:rxdart/rxdart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return CupertinoApp(
       title: 'Rolodex',
       theme: CupertinoThemeData(
         barBackgroundColor: CupertinoDynamicColor.withBrightness(
@@ -19,16 +20,72 @@ class MyApp extends StatelessWidget {
           darkColor: Color(0xFF1D1D1D),
         ),
       ),
-      home: Center(child: TickerView()),
+      home: Center(
+        child: Column(mainAxisSize: MainAxisSize.min, children: [FadeInView()]),
+      ),
     );
   }
 }
 
-class TickerView extends HookWidget {
-  const TickerView({super.key});
+class FadeInView extends HookWidget {
+  const FadeInView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final controller = useAnimationController(
+      duration: const Duration(seconds: 10),
+    );
+
+    useEffect(() {
+      controller.toggle();
+      return null;
+    });
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) => Opacity(
+        opacity: controller.value,
+        child: const FlutterLogo(size: 100),
+      ),
+    );
+  }
+}
+
+class FadeInViewStateful extends StatefulWidget {
+  const FadeInViewStateful({super.key});
+
+  @override
+  State<FadeInViewStateful> createState() => _FadeInViewStatefulState();
+}
+
+class _FadeInViewStatefulState extends State<FadeInViewStateful>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    );
+    controller.toggle();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) => Opacity(
+        opacity: controller.value,
+        child: const FlutterLogo(size: 100),
+      ),
+    );
   }
 }
